c48199340190a31048da3319725b1d07ca533bc4
Ariel Batoon <ariel.batron@growthops.asia>
2024-07-10 15:01:16 +0800

    Done Step 1

---
+++ src/components/main/checklist-popup.vue
@@ -0,0 +1,54 @@
+<script setup>
+const checklistData = {
+    "list": [
+        {
+            "index": 1,
+            "title": "Accident claims",
+            "items": ["Government ID", "Police/narration report", "Official receipts", "Hospital statement of account", "Certificate of employment / medical certificate"]
+        },
+        {
+            "index": 2,
+            "title": "Illness claims",
+            "items": ["Government ID", "Hospital statement of account", "Medical certificate", "Histopath report", "Discharge summary"]
+        },
+        {
+            "index": 3,
+            "title": "Death claims",
+            "items": ["Death certificate of the deceased", "Police/narration report", "Government ID of benefeciary", "Marriage certificate"]
+        },
+    ]
+};
+</script>
+
+<template>
+    <f7-popup class="checklist-popup">
+        <f7-navbar>
+            <f7-nav-left>
+                <f7-link popup-close><svg class="w-[24px] h-[24px] text-gray-800" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                            d="m15 19-7-7 7-7" />
+                    </svg>
+                </f7-link>
+                <span class="text-gray-800 text-base font-medium">Back to submit claim</span>
+            </f7-nav-left>
+        </f7-navbar>
+        <f7-block>
+            <h3 class="text-gray-800 font-bold text-lg uppercase mb-6">Documents Checklist</h3>
+
+            <div class="flex gap-4 mb-4 pb-4" v-for="(data, index) in checklistData.list" :key="data.index"
+                :class="{ 'border-b': index !== checklistData.list.length - 1 }">
+                <p
+                    class="bg-gray-300 p-4 rounded-full flex-none grow-0 shrink-0 w-4 h-4 flex items-center justify-center font-medium text-gray-700">
+                    {{ data.index }}
+                </p>
+                <p>
+                    <span class="font-bold text-gray-700 text-base">{{ data.title }}</span>
+                <ul class="ms-6 text-base text-gray-700">
+                    <li class="list-disc" v-for="list in data.items" :key="list">{{ list }}</li>
+                </ul>
+                </p>
+            </div>
+        </f7-block>
+    </f7-popup>
+</template>
--- src/pages/step1.vue
+++ src/pages/step1.vue
@@ -6,6 +6,7 @@ import Title from '../components/common/title.vue';
 import Subtitle from '../components/common/subtitle.vue';
 import Divider from '../components/common/divider.vue';
 import DocumentInfo from '../components/main/document-info.vue';
+import ChecklistPopup from '../components/main/checklist-popup.vue';
 
 // Form Input Components
 import InputText from '../components/form/input-text.vue';
@@ -43,45 +44,14 @@ const selectedFiles = ref([]);
                                 stroke-width="1.5"
                                 d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
-                        <span class="cursor-pointer hover:underline popup-open" data-popup=".demo-popup">View
+                        <span class="cursor-pointer hover:underline popup-open" data-popup=".checklist-popup">View
                             document checklist</span>
                     </p>
                 </div>
 
-                <f7-popup class="demo-popup" style="height: 60vh;">
-                    <f7-navbar title="DOCUMENTS CHECKLIST">
-                        <f7-nav-right>
-                            <f7-link popup-close>Close</f7-link>
-                        </f7-nav-right>
-                    </f7-navbar>
-                    <f7-block>
-                        <p>
-                            Here comes popup. You can put here anything, even independent view with its own
-                            navigation. Also not, that by default popup looks a bit different on iPhone/iPod and
-                            iPad, on iPhone it is fullscreen.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus mauris
-                            leo, eu bibendum neque congue non. Ut leo mauris, eleifend eu commodo a, egestas ac
-                            urna. Maecenas in lacus faucibus, viverra ipsum pulvinar, molestie arcu. Etiam lacinia
-                            venenatis dignissim. Suspendisse non nisl semper tellus malesuada suscipit eu et eros.
-                            Nulla eu enim quis quam elementum vulputate. Mauris ornare consequat nunc viverra
-                            pellentesque. Aenean semper eu massa sit amet aliquam. Integer et neque sed libero
-                            mollis elementum at vitae ligula. Vestibulum pharetra sed libero sed porttitor.
-                            Suspendisse a faucibus lectus.
-                        </p>
-                        <p>
-                            Duis ut mauris sollicitudin, venenatis nisi sed, luctus ligula. Phasellus blandit nisl
-                            ut lorem semper pharetra. Nullam tortor nibh, suscipit in consequat vel, feugiat sed
-                            quam. Nam risus libero, auctor vel tristique ac, malesuada ut ante. Sed molestie, est in
-                            eleifend sagittis, leo tortor ullamcorper erat, at vulputate eros sapien nec libero.
-                            Mauris dapibus laoreet nibh quis bibendum. Fusce dolor sem, suscipit in iaculis id,
-                            pharetra at urna. Pellentesque tempor congue massa quis faucibus. Vestibulum nunc eros,
-                            convallis blandit dui sit amet, gravida adipiscing libero.
-                        </p>
-                    </f7-block>
-                </f7-popup>
-
+                <!-- Checklist Popup -->
+                <ChecklistPopup />
+                
                 <!-- Click to Upload Action -->
                 <div class="space-y-2">
                     <InputFile v-model="selectedFiles" />
