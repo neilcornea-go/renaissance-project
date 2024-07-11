7e669e75741e6e12b23b61994e4ee766aedc9e5c
Ariel Batoon <ariel.batron@growthops.asia>
2024-07-11 16:50:11 +0800

    Added Step 3

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,6 @@
 import FirstStep from '../pages/step1.vue';
 import SecondStep from '../pages/step2.vue';
+import ThirdStep from '../pages/step3.vue';
 import FourthStep from '../pages/step4.vue';
 
 var routes = [
@@ -14,6 +15,10 @@ var routes = [
   {
     path: '/step-2',
     component: SecondStep,
+  },
+  {
+    path: '/step-3',
+    component: ThirdStep,
   },
   {
     path: '/step-4',
